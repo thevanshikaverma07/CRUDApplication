c2fb322599ad22a46cdbf6bea67317c7a2f32c59
Vanshika Verma <your_email@example.com>
2026-07-22 21:21:40 +0530

    First commit - CRUD Application

---
+++ CRUDAppusingADO/ConnectionString.cs
@@ -0,0 +1,8 @@
+﻿namespace CRUDAppusingADO
+{
+    public  static class ConnectionString
+    {
+        private static string cs = "Server=DESKTOP-L36IGHI\\SQLEXPRESS;Database=CrudADOdb;Trusted_Connection=True;TrustServerCertificate=True;";
+        public static string dbcs { get => cs; }
+    }
+}
---
+++ CRUDAppusingADO/Models/Employees.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAppusingADO.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string Gender { get; set; }
+        
+        [Required]
+        public int Age { get; set; }
+        
+        [Required]
+        public string Designation { get; set; }
+       
+        [Required]
+        public string City { get; set; }
+        public string FullName { get; set; }
+    }
+}
